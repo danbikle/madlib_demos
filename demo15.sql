ad96db3a606a912ca5b4d7c32c004958ca314a4a
ann <ann@ml4.us>
2016-08-21 01:23:22 -0700

    hello

--- demo15.sql
+++ demo15.sql
@@ -94,6 +94,6 @@ SELECT madlib.svm_classification(
 DROP TABLE svm_slpm1_predictions;
 SELECT  madlib.svm_predict('svm_slpm1', 'testdata', 'id', 'svm_slpm1_predictions');
 
-select count(id),min(id),max(id) from svm_slpm1;
+select count(id),min(id),max(id) from svm_slpm1_predictions;
 
 -- bye
