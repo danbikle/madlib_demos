b46e883d2071152534c2f71244ae881b33c9f04f
ann <ann@ml4.us>
2016-08-21 22:16:49 -0700

    hello

---
+++ demo19.bash
@@ -0,0 +1,16 @@
+#!/bin/bash
+
+# demo19.bash
+# This script should collect predictions from several models.
+
+# I should drop predictions from previous run:
+./psqlmad<<EOF
+DROP TABLE IF EXISTS predictions;
+EOF
+
+for ((year=2010; year < 2017 ; year++))
+do
+  echo $year
+  ./psqlmad -f demo19.sql -v tstyr=$year -v trainyrs=30
+done
+exit
