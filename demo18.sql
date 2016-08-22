fb6e20cf870c1d8df595118727d93c4803d10437
ann <ann@ml4.us>
2016-08-21 19:46:50 -0700

    hello

--- demo18.sql
+++ demo18.sql
@@ -101,7 +101,7 @@ SELECT madlib.svm_regression(
 'gaussian',
 'n_components=10',
 '',
-'init_stepsize=[1,0.1,0.01], max_iter=150, n_folds=[9,19] lambda=[0.01,0.02], epsilon=[0.01, 0.02]'
+'init_stepsize=[1,0.1,0.01], max_iter=150, n_folds=20 lambda=[0.01,0.02], epsilon=[0.01, 0.02]'
 );
 -- 'init_stepsize=[1,0.1,0.01], max_iter=[100,150], n_folds=20, lambda=[0.01,0.02], epsilon=[0.01, 0.02]'
 
