488591e49dd32108520ea556252780757a941e7d
ann <ann@ml4.us>
2016-09-08 19:05:41 -0700

    more

--- svmreg11.bash
+++ svmreg11.bash
@@ -3,10 +3,11 @@
 # svmreg11.bash
 
 # This script should use a loop to call svmreg11.sql
-
+./psqlmad -f svmreg11truncate_pred.sql
 for ((year=2000; year < 2017 ; year++))
 do
   echo $year
   ./psqlmad -f svmreg11.sql -v tstyr=$year -v trainyrs=30
+  ./psqlmad -f svmreg11collect_pred.sql
 done
 exit
