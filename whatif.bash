3bab3e58d36db8b544c431951682e0b09f77675e
ann <ann@ml4.us>
2016-09-11 20:01:18 -0700

    more

--- whatif.bash
+++ whatif.bash
@@ -23,6 +23,6 @@ echo $#
 # I should get prices:
 curl http://ichart.finance.yahoo.com/table.csv?s=%5EGSPC > gspc.csv
 
-./psqlmad -af whatif.sql -v whatif_price=$1 -v tstyr=2016 -v trainyrs=25 -v ma1=2 -v ma2=3 -v ma3=4 -v ma4=5 -v ma4=5 -v ma5=6 -v ma6=7 -v ma7=8 -v ma8=9
+./psqlmad -af whatif.sql -v whatif_price=$1 -v tstyr=`date +%Y` -v trainyrs=25 -v ma1=2 -v ma2=3 -v ma3=4 -v ma4=5 -v ma4=5 -v ma5=6 -v ma6=7 -v ma7=8 -v ma8=9
 
 exit
